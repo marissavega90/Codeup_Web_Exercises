0e2bfeb15627565f04a5d1d336a356035eea073c
Marissa <marissavega90@gmail.com>
2014-12-15 11:13:31 -0600

    Added iterating arrays in JavaScript using for each loop

--- public/js/iterating_arrays.js
+++ public/js/iterating_arrays.js
@@ -6,16 +6,20 @@
 
 var names = ['Isaac', 'Cory', 'Jill', 'Thomas'];
 
-// todo:
-// Create a log statement that will log the number of elements in the names array.
-console.log('There are ' + names.length + ' names in the array');
+// // todo:
+// // Create a log statement that will log the number of elements in the names array.
+// console.log('There are ' + names.length + ' names in the array');
 
-// todo:
-// Create log statements that will print each of the names array elements individually.
-console.log('The first name is ' + names[0]);
+// // todo:
+// // Create log statements that will print each of the names array elements individually.
+// console.log('The first name is ' + names[0]);
 
-console.log('The second name is ' + names[1]);
+// console.log('The second name is ' + names[1]);
 
-console.log('The third name is ' + names[2]);
+// console.log('The third name is ' + names[2]);
 
-console.log('The fourth name is ' + names[3]);
+// console.log('The fourth name is ' + names[3]);
+
+names.forEach(function (name, index, array) {
+    console.log('The name at ' + index + ' is: ' + name);
+});
